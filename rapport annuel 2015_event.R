4a0c21f0d21fc05c22721b0be95244c71d9df4d8
Thierry POLARD <thierry.polard@lyonnaise-des-eaux.fr>
2016-02-22 18:53:03 +0100

    + optimisation mnineures, + amélioraations graphiques

--- rapport annuel 2015_event.R
+++ rapport annuel 2015_event.R
@@ -11,23 +11,22 @@ rm(list=ls())
 ## définition de l'opération "ni" (l'inverse de "in")
 `%ni%` <- Negate(`%in%`)
 
-#install.packages("openxlsx")
-#install.packages("ggthemes")
-
-
 ### chargement packages
-require("ggplot2")   ## pour faire des jolis graphs
-library("cowplot")   ## pour  afficher plusieurs ggplot
-library("fortunes")  ##
-library("lattice") 
-library("ggthemes")
-library("grid")      ## 
-library("openxlsx")
-library("plotly")
-library("xts")
-library("openair")
-library("Cairo" ) ## pour exporter correctement les graphs
-library("reshape2")
+ma_liste_de_packages <- c("ggplot2", ## pour faire des jolis graphs
+        "cowplot", ## pour  afficher plusieurs ggplot
+        "fortunes", 
+        "lattice", 
+        "ggthemes", 
+        "openxlsx", ## pour ouvrir directement les fichier excel
+        "plotly", ## pour des graph interactifs
+        "xts", 
+        "openair",
+        "Cairo" , ## pour exporter correctement les graphs
+        "reshape2" ) 
+
+lapply(ma_liste_de_packages, require, character.only = TRUE) ## require sur l'ensemble des packages
+
+
 
 #theme_set(theme_gray())
 
@@ -37,7 +36,13 @@ fortune(15)
 
 #####################
 
-## import des mesure
+
+col_thil    <-"#208CFF"
+col_reserve <- "#0DB219"
+
+
+
+##### import des mesures
 
 setwd("C:/Users/XZ4062/Desktop/LyRE/Monitoring/Sirenes/2_BDX_M/Valorisation des donnees/1_données brutes/Rapport annuel 2015/evenement")
 getwd()
@@ -86,29 +91,19 @@ annees <- c(2014,2015)
 
 ## format date
 
-#oxygene
 n <- length(sites)
 m <- length(annees)
 for (i in 1:n){
   for (j in 1:m){
-    eval(parse(text=paste0("",sites[i],"_",annees[j],"_event_ox_full$date <- convertToDateTime(",sites[i],"_",annees[j],"_event_ox_full$date)"))) ###??? fonction de openxlsx
+   eval(parse(text=paste0("",sites[i],"_",annees[j],"_event_ox_full$date <- convertToDateTime(",sites[i],"_",annees[j],"_event_ox_full$date)"))) ###??? fonction de openxlsx
    eval(parse(text=paste0("",sites[i],"_",annees[j],"_event_ox_full$date <- round(",sites[i],"_",annees[j],"_event_ox_full$date,\"mins\")"))) ## besoin d'arrondir les date à la minute pres, car pb d'arrondi avec la conversion précédente
-   }
+   
+   eval(parse(text=paste0("",sites[i],"_",annees[j],"_event_cond_full$date <- convertToDateTime(",sites[i],"_",annees[j],"_event_cond_full$date)"))) ###??? fonction de openxlsx
+   eval(parse(text=paste0("",sites[i],"_",annees[j],"_event_cond_full$date <- round(",sites[i],"_",annees[j],"_event_cond_full$date,\"mins\")"))) ## besoin d'arrondir les date à la minute pres, car pb d'arrondi avec la conversion précédente
+         }
 }
 
-# conductivte
-## format date
-n <- length(sites)
-m <- length(annees)
-for (i in 1:n){
-  for (j in 1:m){
-    eval(parse(text=paste0("",sites[i],"_",annees[j],"_event_cond_full$date <- convertToDateTime(",sites[i],"_",annees[j],"_event_cond_full$date)"))) ###??? fonction de openxlsx
-    eval(parse(text=paste0("",sites[i],"_",annees[j],"_event_cond_full$date <- round(",sites[i],"_",annees[j],"_event_cond_full$date,\"mins\")"))) ## besoin d'arrondir les date à la minute pres, car pb d'arrondi avec la conversion précédente
-  }
-}
-
-
-## format num
+## format num # pour les paramètres qui ne sont pas chargés correctement
 n <- length(sites)
 for (i in 1:n){
   eval(parse(text=paste0("",sites[i],"_2015_event_ox_full$filtre <- as.numeric(",sites[i],"_2015_event_ox_full$filtre)"))) 
@@ -116,7 +111,7 @@ for (i in 1:n){
 }
 
 
-## allegement de la table à manipuler (bcp de colonne inutiles)
+## allegement de la table à manipuler (bcp de colonne inutiles) 
 
 #???oxygene
 n <- length(sites)
@@ -145,28 +140,27 @@ for (i in 1:n){
 ################# import des données d'évenement et création des layers ggplot associés
 
 ## Import des données  d'évenement
-thil_2015_event<- read.xlsx("evenement.xlsx", sheet = 1, startRow = 1, colNames = TRUE, 
+thil_event<- read.xlsx("evenement.xlsx", sheet = 1, startRow = 1, colNames = TRUE, 
                             rowNames = FALSE, detectDates = F, skipEmptyRows = FALSE,
                             rows = NULL, cols = NULL, check.names = FALSE, namedRegion = NULL)
 
-thil_2015_event[,2] <-as.POSIXct( round(convertToDateTime(thil_2015_event[,2]),"mins"),format="%d/%m/%Y %H:%M",tz="GMT")
-thil_2015_event[,3] <-as.POSIXct( round(convertToDateTime(thil_2015_event[,3]),"mins"),format="%d/%m/%Y %H:%M",tz="GMT")
+reserve_event<- read.xlsx("evenement.xlsx", sheet = 2, startRow = 1, colNames = TRUE, 
+                            rowNames = FALSE, detectDates = F, skipEmptyRows = FALSE,
+                            rows = NULL, cols = NULL, check.names = FALSE, namedRegion = NULL)
 
-
-reserve_2015_event<- read.xlsx("evenement.xlsx", sheet = 2, startRow = 1, colNames = TRUE, 
-                               rowNames = FALSE, detectDates = F, skipEmptyRows = FALSE,
-                               rows = NULL, cols = NULL, check.names = FALSE, namedRegion = NULL)
-
-reserve_2015_event[,2] <-as.POSIXct( round(convertToDateTime(reserve_2015_event[,2]),"mins"),format="%d/%m/%Y %H:%M",tz="GMT")
-reserve_2015_event[,3] <-as.POSIXct( round(convertToDateTime(reserve_2015_event[,3]),"mins"),format="%d/%m/%Y %H:%M",tz="GMT")
-
+## conversion des information de début et fin d'évenement au bon format date
+n <- length(sites)
+for (i in 1:n){
+    eval(parse(text=paste0("",sites[i],"_event[,2] <-as.POSIXct( round(convertToDateTime(",sites[i],"_event[,2]),\"mins\"),format=\"%d/%m/%Y %H:%M\",tz=\"GMT\")")))
+    eval(parse(text=paste0("",sites[i],"_event[,3] <-as.POSIXct( round(convertToDateTime(",sites[i],"_event[,3]),\"mins\"),format=\"%d/%m/%Y %H:%M\",tz=\"GMT\")")))
+}
 
 ## création de la charte visuelle pour matérialiser les évenement (création d'instance layer)
-
-n <- length(thil_2015_event$Id)
+#### compliqué d'optimiser car besoin d'appeller le nom du site 
+n <- length(thil_event$Id)
 for (i in 1:n){
-  eval(parse(text=paste0("a_thil",i," <- thil_2015_event[",i,",2]")))
-  eval(parse(text=paste0("b_thil",i," <- thil_2015_event[",i,",3]")))
+  eval(parse(text=paste0("a_thil",i," <- thil_event[",i,",2]")))
+  eval(parse(text=paste0("b_thil",i," <- thil_event[",i,",3]")))
   eval(parse(text=paste0("data<- geom_rect(aes(xmin = a_thil",i,", 
                          xmax = b_thil",i,",
                          ymin = -Inf, ymax = +Inf,
@@ -175,18 +169,18 @@ for (i in 1:n){
   print("ok") 
   assign(paste("G_thil",i,sep="_"),data,.GlobalEnv)
 }
-
-
 liste_temp_thil <- c()
 liste_temp_thil <- paste0("G_thil_",paste0(1:n))
 viz_event_thil  <- c()
 viz_event_thil  <- paste(liste_temp_thil,collapse=" + ")
 
+
+
 ###
-n <- length(reserve_2015_event$Id)
+n <- length(reserve_event$Id)
 for (i in 1:n){
-  eval(parse(text=paste0("a_reserve",i," <- reserve_2015_event[",i,",2]")))
-  eval(parse(text=paste0("b_reserve",i," <- reserve_2015_event[",i,",3]")))
+  eval(parse(text=paste0("a_reserve",i," <- reserve_event[",i,",2]")))
+  eval(parse(text=paste0("b_reserve",i," <- reserve_event[",i,",3]")))
   eval(parse(text=paste0("data<- geom_rect(aes(xmin = a_reserve",i,", 
                          xmax = b_reserve",i,",
                          ymin = -Inf, ymax = +Inf,
@@ -195,7 +189,6 @@ for (i in 1:n){
   print("ok") 
   assign(paste("G_reserve",i,sep="_"),data,.GlobalEnv)
 }
-
 
 liste_temp_reserve <- c()
 liste_temp_reserve <- paste0("G_reserve_",paste0(1:n))
@@ -208,7 +201,7 @@ viz_event_reserve  <- paste(liste_temp_reserve,collapse=" + ")
 
 
 
-###  cadrage temporel (on veut un graph par trimestre) ### a améliorer
+###  cadrage temporel (on veut un graph par trimestre) ### a améliorer mais je ne vois pas trop comment
 debut_T1_2015 <- as.POSIXct("01/01/2015 00:00", format="%d/%m/%Y %H:%M",tz="GMT")
 fin_T1_2015   <- as.POSIXct("31/03/2015 23:45", format="%d/%m/%Y %H:%M",tz="GMT")
 
@@ -249,8 +242,6 @@ daterange_2014<- list(T1=c(debut_T1_2014,fin_T1_2014),
                       T4=c(debut_T4_2014,fin_T4_2014)
 )
 
-
-
 ####### oxygene (création des graphs)
 
 ## fonction graph Amplitude
@@ -261,12 +252,14 @@ graph_T_amp <- function(site,annee,param){
     event <- eval(parse(text=paste0("viz_event_",site,"")))
     eval(parse(text=paste0("p_T",i," <- ggplot() + 
                            geom_line (data = ",site,"_",annee,"_event_",param,", 
-                                      aes(x = date, y = filtre)) +
-                           geom_point(data = ",site,"_",annee,"_event_",param,",
-                                      aes(x = date, y =Signale_amplitude,colour=Intensite_signal_amplitude),
-                                      size=3)+
-                           scale_color_gradient(low=\"red\", high=\"green\") +
+                                      aes(x = date, y = filtre),
+                                      colour=col_",site,",size = 1, alpha =0.5) +
                            ",event," +
+                           geom_point(data = ",site,"_",annee,"_event_",param,",
+                                      aes(x = date, y =Signale_amplitude,colour=Intensite_signal_amplitude),size=3)+
+                           scale_color_gradient(name =\"baisse de\nl'amplitude journaliere\",low=\"red\", high=\"green\") +
+                           theme(axis.title.x = element_blank()) + 
+                           ylab(\"\")  +
                            xlim(daterange_",annee,"$T",i,") ")
                            ))
   }
@@ -274,6 +267,11 @@ graph_T_amp <- function(site,annee,param){
   liste_graph <-paste(liste_temp,collapse=",")
   eval(parse(text=paste0("plot_grid(",liste_graph,", nrow=",n,", align = \"v\")")))
 }
+
+
+## exemple
+graph_T_amp("reserve", "2015", "ox")
+graph_T_amp("thil", "2015", "ox")
 
 
 ## fonction graph niveau Max
@@ -284,12 +282,15 @@ graph_T_max <- function(site,annee,param){
     event <- eval(parse(text=paste0("viz_event_",site,"")))
     eval(parse(text=paste0("p_T",i," <- ggplot() + 
                            geom_line (data = ",site,"_",annee,"_event_",param,", 
-                                      aes(x = date, y = filtre)) +
+                                      aes(x = date, y = filtre),
+                                      colour=col_",site,",size = 1, alpha =0.5) +
+                           ",event," +
                            geom_point(data = ",site,"_",annee,"_event_",param,",
                                       aes(x = date, y =Signale_niveau,colour=Intensite_signal_niveau),
                                       size=3)+
-                           scale_color_gradient(low=\"red\", high=\"green\") +
-                           ",event," +
+                           scale_color_gradient(name =\"baisse du\nmaximum journalier\", low=\"red\", high=\"green\") +
+                           theme(axis.title.x = element_blank()) + 
+                           ylab(\"\")  +
                            xlim(daterange_",annee,"$T",i,") ")
     ))
   }
@@ -298,9 +299,11 @@ graph_T_max <- function(site,annee,param){
   eval(parse(text=paste0("plot_grid(",liste_graph,", nrow=",n,", align = \"v\")")))
 }
 
+
+
 ## exemple
-graph_T_amp("thil", "2015", "ox")
-graph_T_max("reserve", "2014", "ox")
+graph_T_max("thil", "2015", "ox")
+graph_T_max("reserve", "2015", "ox")
 
 
 
@@ -313,11 +316,15 @@ graph_T_cond <- function(site,annee,param){
   for (i in 1:n){
     event <- eval(parse(text=paste0("viz_event_",site,"")))
     eval(parse(text=paste0("p_T",i," <- ggplot() + 
-                           geom_line (data = ",site,"_",annee,"_event_",param,", aes(x = date, y = non_signalees)) +
+                           geom_line (data = ",site,"_",annee,"_event_",param,", 
+                                      aes(x = date, y = non_signalees),
+                                      colour=col_",site,",size = 1, alpha =0.5) +
+                           ",event," +
                            geom_point(data = ",site,"_",annee,"_event_",param,", aes(x = date, y =signal_precoce), size=2)+
                            geom_point(data = ",site,"_",annee,"_event_",param,", aes(x = date, y =signal_avere,colour=intensite_du_signal), size=3)+
-                           scale_color_gradient(low=\"green\", high=\"red\") +
-                           ",event," +
+                           scale_color_gradient(name =\"baisse de la\nconductivité\",low=\"green\", high=\"red\") +
+                           theme(axis.title.x = element_blank()) + 
+                           ylab(\"\")  +
                            xlim(daterange_",annee,"$T",i,") ")
                            ))
   }
@@ -325,41 +332,7 @@ graph_T_cond <- function(site,annee,param){
   liste_graph <-paste(liste_temp,collapse=",")
   eval(parse(text=paste0("plot_grid(",liste_graph,", nrow=",n,", align = \"v\")")))
   }
-##########
 
-
+## exemple
 graph_T_cond("thil", "2015", "cond")
-
-
-
-
-
-
-
-## modification de la fonction de création de grapj pour qu'lle inclue également la matérialisation des évenements
-
-#####
-graph_T_max_E <- function(site,annee,param){
-  x11()
-  for (j in 1:4){        ## nombr de trimestre dans chaque année - utilisation de j car i est déjà apellé dans "viz event"
-    event <- eval(parse(text=paste0("viz_event_",site,"")))
-    eval(parse(text=paste0("p_T",j," <- ggplot() + 
-                           geom_line (data = ",site,"_",annee,"_event_",param,", 
-                           aes(x = date, y = filtre)) +
-                           geom_point(data = ",site,"_",annee,"_event_",param,",
-                           aes(x = date, y =Signale_niveau,colour=Intensite_signal_niveau),size=3)+
-                           scale_color_gradient(low=\"red\", high=\"green\") +
-                           ",event," +
-                           xlim(daterange_",annee,"$T",j,") ")
-                           ))
-  }
-  liste_temp <- paste0("p_T",paste0(1:4))
-  liste_graph <-paste(liste_temp,collapse=",")
-  eval(parse(text=paste0("plot_grid(",liste_graph,", nrow=",4,", align = \"v\")")))
-  }
-######
-
-graph_T_max_E("thil", "2015", "ox")
-graph_T_max_E("reserve", "2015", "ox")
-
-
+graph_T_cond("reserve", "2015", "cond")
